e1e214b98037e3e2e5b59ac18f6606e79fca0ed0
beekhnlor <beekingwordviengxay@gmail.com>
2023-12-07 08:33:15 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:chapter7_khonesavanh_2cw3/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyWidget(),
+    );
+  }
+}
+
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: [home()],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
